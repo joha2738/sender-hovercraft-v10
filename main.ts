ff712a36982859e62ac8d292f8c0870908850307
joha2738 <65940893+joha2738@users.noreply.github.com>
2021-05-16 23:15:18 +0200

    De første filer til MakeCode-projekt

---
+++ main.ts
@@ -0,0 +1,26 @@
+/**
+ * PIN1 == Fremdrift (Gul, IN1/IN2)
+ * 
+ * PIN0 == Opdrift    (Grøn, IN3/IN4)
+ * 
+ * PIN2 == Servo (Blå)
+ */
+input.onGesture(Gesture.Shake, function () {
+    radio.sendValue("A", 0)
+    basic.showString("0")
+})
+input.onButtonPressed(Button.A, function () {
+	
+})
+input.onButtonPressed(Button.AB, function () {
+    radio.sendValue("A", 1)
+    basic.showString("1")
+})
+input.onButtonPressed(Button.B, function () {
+	
+})
+radio.setGroup(107)
+radio.setTransmitPower(7)
+basic.forever(function () {
+	
+})
