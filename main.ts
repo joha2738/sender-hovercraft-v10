3e85061badc45052a460f85c67de9ce0fede3122
joha2738 <65940893+joha2738@users.noreply.github.com>
2021-05-16 23:19:49 +0200

    Opdater main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,10 +1,3 @@
-/**
- * PIN1 == Fremdrift (Gul, IN1/IN2)
- * 
- * PIN0 == Opdrift    (Grøn, IN3/IN4)
- * 
- * PIN2 == Servo (Blå)
- */
 input.onGesture(Gesture.Shake, function () {
     radio.sendValue("A", 0)
     basic.showString("0")
@@ -21,6 +14,15 @@ input.onButtonPressed(Button.B, function () {
 })
 radio.setGroup(107)
 radio.setTransmitPower(7)
+/**
+ * PIN1 == Fremdrift (Gul, IN1/IN2)
+ * 
+ * PIN0 == Opdrift    (Grøn, IN3/IN4)
+ * 
+ * PIN2 == Servo (Blå)
+ */
 basic.forever(function () {
-	
+    if (input.rotation(Rotation.Pitch) < 80) {
+    	
+    }
 })
